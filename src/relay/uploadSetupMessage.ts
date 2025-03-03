bfda84057b496056d5b452b0978eb0d6f46a413a
Raghav Sood <r@raghavsood.com>
2025-03-04 03:24:59 +0800

    relay: basic message structures

---
+++ src/relay/uploadSetupMessage.ts
@@ -0,0 +1,43 @@
+type UploadSetupMessageInput = {
+  serverUrl: string
+  message: string
+  sessionId: string
+  messageId?: string
+  additionalHeaders?: string
+}
+/**
+ *
+ * @param {UploadSetupMessageInput} input - the input parameters
+ * @throws {Error} - if the response status is not between 200 and 300
+ */
+export const uploadSetupMessage = async ({
+  serverUrl,
+  message,
+  sessionId,
+  messageId,
+  additionalHeaders,
+}: UploadSetupMessageInput): Promise<void> => {
+  const headers: Record<string, string> = {
+    'Content-Type': 'application/json',
+  }
+  if (messageId) {
+    headers['message_id'] = messageId
+  }
+  // this one is only temporary right now
+  // when we do dkls/schnorr reshare will need it
+  if (additionalHeaders) {
+    headers['message-id'] = additionalHeaders
+  }
+
+  const response = await fetch(`${serverUrl}/setup-message/${sessionId}`, {
+    method: 'POST',
+    headers,
+    body: message,
+  })
+
+  if (response.status < 200 || response.status > 299) {
+    throw new Error(
+      `failed to upload setup message to relay server, status: ${response.status}`
+    )
+  }
+}
