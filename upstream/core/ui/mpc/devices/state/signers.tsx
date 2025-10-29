538f1b49176fb1407dfea9c3dfa2e07d1f1b7c94
Born Slippy <bornslippynuxx@proton.me>
2025-10-29 12:01:00 -0600

    stop tracking upstream directory

--- upstream/core/ui/mpc/devices/state/signers.tsx
+++
@@ -1,4 +0,0 @@
-import { getValueProviderSetup } from '@lib/ui/state/getValueProviderSetup'
-
-export const { useValue: useMpcSigners, provider: MpcSignersProvider } =
-  getValueProviderSetup<string[]>('MpcSigners')
