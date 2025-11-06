26ce46f8c3cd2643021d7f19af02bb4b366e29e2
Born Slippy <bornslippynuxx@proton.me>
2025-11-06 07:29:48 -0700

    sync-and-copy changes

--- packages/core/chain/feeQuote/resolvers/ripple.ts
+++
@@ -1,7 +0,0 @@
-import { rippleTxFee } from '../../tx/fee/ripple'
-
-import { FeeQuoteResolver } from '../resolver'
-
-export const getRippleFeeQuote: FeeQuoteResolver<'ripple'> = async () => {
-  return { gas: BigInt(rippleTxFee) }
-}
