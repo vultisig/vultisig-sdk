3a40960cc6391b69bbe6371874889b64399d64b9
rcoderdev <radzionch@gmail.com>
2026-07-17 00:55:36 +0400

    fix(swap): align provider registries (#1271)
    * Align swap provider registries
    
    * Add SDK release changeset
    
    ---------
    
    Co-authored-by: gomes <17035424+gomesalexandre@users.noreply.github.com>

--- packages/core/chain/swap/swapEnabledChains.ts
+++ packages/core/chain/swap/swapEnabledChains.ts
@@ -1,3 +1,6 @@
+import { cowSwapSupportedChains } from '@vultisig/core-chain/swap/general/cowswap/config'
+import { jupiterSwapEnabledChains } from '@vultisig/core-chain/swap/general/jupiter/JupiterSwapEnabledChains'
+import { kyberSwapEnabledChains } from '@vultisig/core-chain/swap/general/kyber/chains'
 import { lifiSwapEnabledChains } from '@vultisig/core-chain/swap/general/lifi/LifiSwapEnabledChains'
 import { oneInchSwapEnabledChains } from '@vultisig/core-chain/swap/general/oneInch/OneInchSwapEnabledChains'
 import { swapKitEnabledChains } from '@vultisig/core-chain/swap/general/swapkit/SwapKitEnabledChains'
@@ -5,7 +8,10 @@ import { nativeSwapEnabledChains } from '@vultisig/core-chain/swap/native/Native
 
 export const swapEnabledChains = [
   ...nativeSwapEnabledChains,
+  ...cowSwapSupportedChains,
+  ...kyberSwapEnabledChains,
   ...oneInchSwapEnabledChains,
+  ...jupiterSwapEnabledChains,
   ...lifiSwapEnabledChains,
   ...swapKitEnabledChains,
 ] as const
