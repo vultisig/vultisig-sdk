a160f3047d7bcca2b4158549fb0f97c1294384a1
gomesalexandre <17035424+gomesalexandre@users.noreply.github.com>
2026-08-29 09:52:05 +0200

    style(sdk): format payments export barrel

--- packages/sdk/src/tools/payments/index.ts
+++ packages/sdk/src/tools/payments/index.ts
@@ -1,8 +1,4 @@
-export type {
-  CheckoutUsdcChain,
-  DecodedApproveParams,
-  DecodedDepositWithMemoParams,
-} from "./usdcCalldata";
+export type { CheckoutUsdcChain, DecodedApproveParams, DecodedDepositWithMemoParams } from './usdcCalldata'
 export {
   AGENT_ROUTER_ADDRESS,
   APPROVE_SELECTOR,
@@ -19,4 +15,4 @@ export {
   resolveUsdcContract,
   ROUTER_VERSION_PINNED,
   USDC_CONTRACTS,
-} from "./usdcCalldata";
+} from './usdcCalldata'
